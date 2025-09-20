39eb0c367dc19671bae5fdb197320714661b9c05
besho-122 <m123456789@gmail.com>
2025-09-20 20:52:49 +0300

    commit 2

--- Besho.PL/Controllers/CategoryController.cs
+++ Besho.PL/Controllers/CategoryController.cs
@@ -11,20 +11,21 @@ namespace Besho.PL.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        private readonly ICategoryService categoryService;
+        private readonly ICategoryService _categoryService;
 
         public CategoryController(ICategoryService categoryService)
         {
-            this.categoryService = categoryService;
+           
+            this._categoryService = categoryService;
         }
         [HttpGet("")]
         public IActionResult GetAll() {
-            return Ok(categoryService.GetAllCategories());
+            return Ok(_categoryService.GetAllCategories());
         }
 
         [HttpGet("{id}")]
         public IActionResult Get([FromRoute]int id) {
-            var category = categoryService.GetCategoryById(id);
+            var category = _categoryService.GetCategoryById(id);
             if (category is null) return NotFound(new {message="category not found"});
             return Ok(category);
         }
@@ -32,22 +33,22 @@ namespace Besho.PL.Controllers
         [HttpPost]
         public IActionResult Create([FromBody]CategoryRequest request)
         {
-           var id= categoryService.CrateCategory(request);
-            return CreatedAtAction(nameof(Get),new {id});
+           var id= _categoryService.CrateCategory(request);
+            return CreatedAtAction(nameof(Get),new {id},new { message = "created succesfully",request});
            
         }
 
         [HttpPatch("{id}")]
         public IActionResult Update([FromRoute] int id , CategoryRequest request)
         {
-            var updated = categoryService.UpdateCategory(id, request);
+            var updated = _categoryService.UpdateCategory(id, request);
             return updated > 0 ? Ok(new {massage="Category Updated Succesfully"}) : NotFound(new {massage="Categort Not Found to Update it "});
 
         }
 
         [HttpPatch("{id}/toggle-status")]
         public IActionResult ToggleStatus([FromRoute] int id) {
-            bool cat = categoryService.ToggleStatus(id);
+            bool cat = _categoryService.ToggleStatus(id);
             return cat ? Ok(new {message="Category Status Updated Succesfully"}) : NotFound(new {message="Category Not Found to Update It's Status "});
 
         }
@@ -55,7 +56,7 @@ namespace Besho.PL.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete([FromRoute]int id)
         {
-            var delete = categoryService.DeleteCategory(id);
+            var delete = _categoryService.DeleteCategory(id);
             return delete > 0 ? Ok(new {message="Category deleted succesfully",delete}) : NotFound("Category Not Found To Delete It");
 
         }
