f168303159ef6ebc49d4e766a99058ecc21f22e1
besho-122 <m123456789@gmail.com>
2025-09-20 17:28:35 +0300

    Add project files.

---
+++ Besho.PL/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Besho.DAL.Models;
+using Besho.DAL.Data;
+
+namespace Besho.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+
+}
